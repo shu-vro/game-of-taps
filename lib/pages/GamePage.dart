62c161328b5f60e2bf70e69b3347a2b076f5ba26
shu-vro <official.shirshen@gmail.com>
2024-12-27 19:24:19 +0600

    feat: prevent back button to certain grades

--- lib/pages/GamePage.dart
+++ lib/pages/GamePage.dart
@@ -14,92 +14,119 @@ MaterialPageRoute<void> DeclareWinner(String color) {
     fullscreenDialog: true,
     maintainState: false,
     builder: (BuildContext context) {
-      return Scaffold(
-        backgroundColor: color == "Red" ? Colors.amber : Colors.lightBlue,
-        appBar: AppBar(
-          title: Text(''),
-          backgroundColor: color == "Red" ? Colors.amber : Colors.lightBlue,
-          leading: IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$color Team wins!',
-                style: TextStyle(
-                  fontFamily: "Ingrid_Darling",
-                  fontSize: 80,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black,
-                      blurRadius: 40,
-                      offset: Offset(-10, 10),
-                    )
-                  ],
-                ),
+      return PopScope(
+          canPop: false,
+          onPopInvokedWithResult: (bool didPop, final dynamic) async {
+            if (didPop) return;
+            final bool shouldPop = await showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Alert'),
+                    content: Text('Go back to home?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(false),
+                        child: Text('No'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pushNamed(context, "/"),
+                        child: Text('Yes'),
+                      ),
+                    ],
+                  ),
+                ) ??
+                false;
+
+            if (context.mounted && shouldPop) {
+              Navigator.pushNamed(context, "/");
+            }
+          },
+          child: Scaffold(
+            backgroundColor: color == "Red" ? Colors.amber : Colors.lightBlue,
+            appBar: AppBar(
+              title: Text(''),
+              backgroundColor: color == "Red" ? Colors.amber : Colors.lightBlue,
+              leading: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
               ),
-              Row(
+            ),
+            body: Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, "/");
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color.fromARGB(131, 0, 0, 0),
-                            blurRadius: 5,
-                            spreadRadius: 2,
-                            offset: Offset(0, 10),
-                          ),
-                        ],
-                      ),
-                      child: Image.asset(
-                        "assets/images/exit-button.png",
-                        width: 150,
-                      ),
+                  Text(
+                    '$color Team wins!',
+                    style: TextStyle(
+                      fontFamily: "Ingrid_Darling",
+                      fontSize: 80,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black,
+                          blurRadius: 40,
+                          offset: Offset(-10, 10),
+                        )
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    width: 22,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, "/game");
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color.fromARGB(131, 0, 0, 0),
-                            blurRadius: 5,
-                            spreadRadius: 2,
-                            offset: Offset(0, 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, "/");
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(131, 0, 0, 0),
+                                blurRadius: 5,
+                                spreadRadius: 2,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
                           ),
-                        ],
+                          child: Image.asset(
+                            "assets/images/exit-button.png",
+                            width: 150,
+                          ),
+                        ),
                       ),
-                      child: Image.asset(
-                        "assets/images/replay-button.png",
-                        width: 150,
+                      SizedBox(
+                        width: 22,
                       ),
-                    ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, "/game");
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(131, 0, 0, 0),
+                                blurRadius: 5,
+                                spreadRadius: 2,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Image.asset(
+                            "assets/images/replay-button.png",
+                            width: 150,
+                          ),
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
-          ),
-        ),
-      );
+              ),
+            ),
+          ));
     },
   );
 }
